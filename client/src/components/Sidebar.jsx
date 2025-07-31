fa657916f6f135c0937d4bdac3acb85771dccd0d
Danish Ahmed <ahmeddanish6000@gmail.com>
2025-07-31 16:48:50 +0500

    New Additions

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import assets from '../assets/assets'
-import {useNavigate} from 'react-router-dom'
+import assets, { userDummyData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
 
@@ -27,7 +27,29 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
         <input type="text" className='bg-transparent border-none outline-none 
         text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...' />
       </div>
-
+    </div>
+    <div className='flex flex-col'>
+      {userDummyData.map((user, index)=>(
+        <div onClick={()=>{setSelectedUser(user)}} 
+           key={index} className={`relative flex items-center gap-2 p-2 pl-2 rounded 
+           cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50' }`}>
+          <img src={user?.profilePic || assets.avatar_icon} alt="" 
+          className='w-[35px] aspect-[1/1] rounded-full' />
+          <div className='flex flex-col leading-5'>
+            <p>{user.fullName}</p>
+            {
+              index < 3 
+              ? <span className='text-green-400 text-xs'>Online</span> 
+              : <span className='text-neutral-400 text-xs'>Offline</span>
+            }
+          </div>
+          {index > 2 && (
+            <span className="ml-auto text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+              {index}
+            </span>
+          )}
+        </div>
+      ) )}
     </div>
   </div>
 
