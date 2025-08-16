208d3b95a9ee85e1cc28b6048bf0d0db489dc402
Danish Ahmed <ahmeddanish6000@gmail.com>
2025-08-16 23:54:44 +0500

    Some Changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,11 +12,11 @@ const HomePage = () => {
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
       overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
-      'md:grid-cols-[1.2fr_2fr_1fr] xl:grid-cols-[1.5fr_3fr_1.2fr]' : 'md:grid-cols-2'}
+      'md:grid-cols-[1.2fr_2fr_1fr] xl:grid-cols-[1.5fr_3fr_1.2fr]' : 'md:grid-cols-[1fr_1fr_1fr]'}
       `}>
       <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-      <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+      <RightSidebar />
       </div>
     </div>
   )
